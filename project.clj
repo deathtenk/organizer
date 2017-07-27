57cf4c16d422eded573c2e4ae869417f44d0b896
Henk Reder <henk@yetanalytics.com>
2017-07-27 13:08:25 -0400

    added tooling config to project.clj

--- project.clj
+++ project.clj
@@ -16,10 +16,14 @@
 
   :profiles
   {:dev
-   {:dependencies [[binaryage/devtools "0.8.2"]]
+   {:dependencies [[binaryage/devtools "0.8.2"]
+                   [figwheel-sidecar "0.5.8"]
+                   [com.cemerick/piggieback "0.2.1"]]
 
     :plugins      [[lein-figwheel "0.5.9"]]
     }}
+
+    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
   :cljsbuild
   {:builds
