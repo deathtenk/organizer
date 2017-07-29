64a1a364147d7ac04a47998cfc141788de443217
Henk Reder <henk@yetanalytics.com>
2017-07-28 23:46:03 -0400

    added delete event

--- src/cljs/organizer/events.cljs
+++ src/cljs/organizer/events.cljs
@@ -12,3 +12,9 @@
   (fn [{:keys [todo] :as db} [_ event]] 
     (let [m-todo (conj todo event)]
      (assoc db :todo m-todo))))
+
+(re-frame/reg-event-db
+  :delete-event
+  (fn [{:keys [todo] :as db} [_ id]]
+    (let [m-todo (remove #(= (:id %) id) todo)]
+      (assoc db :todo m-todo))))
